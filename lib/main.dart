d1f8f8466f962be3e3ed8cd2455c67187f89fea4
Charlie <creasehuggett@gmail.com>
2021-09-30 20:10:15 +0100

    added image and table

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,196 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MyApp extends StatefulWidget {
-  @override
-  _TableExample createState() => _TableExample();
-}
+import 'dart:async';
+import 'dart:convert' as convert;
+import 'dart:io';
+import 'dart:developer';
 
-class _TableExample extends State<MyApp> {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Chelsea Fc'),
-          ),
-          body: Center(
-              child: Column(children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(20),
-              child: Table(
-                defaultColumnWidth: FixedColumnWidth(120.0),
-                border: TableBorder.all(
-                    color: Colors.black, style: BorderStyle.solid, width: 2),
-                children: [
-                  TableRow(children: [
-                    Column(children: [
-                      Text('Details', style: TextStyle(fontSize: 20.0))
-                    ]),
-                    Column(children: [
-                      Text('Value', style: TextStyle(fontSize: 20.0))
-                    ]),
-                  ]),
-                  TableRow(children: [
-                    Column(children: [Text('Team Name')]),
-                    Column(children: [Text('Chelsea')]),
-                  ]),
-                  TableRow(children: [
-                    Column(children: [Text('Manager')]),
-                    Column(children: [Text('Tuchel')]),
-                  ]),
-                  TableRow(children: [
-                    Column(children: [Text('Wins')]),
-                    Column(children: [Text('5')])
-                  ]),
-                  TableRow(children: [
-                    Column(children: [Text('Draws')]),
-                    Column(children: [Text('1')])
-                  ]),
-                  TableRow(children: [
-                    Column(children: [Text('Loses')]),
-                    Column(children: [Text('0')])
-                  ]),
-                  TableRow(children: [
-                    Column(children: [Text('Points')]),
-                    Column(children: [Text('16')])
-                  ]),
-                ],
-              ),
-            ),
-          ]))),
+    return MaterialApp(title: 'Startup Name Gen', home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Future<List> func_name() async {
+    http.Response response =
+        await http.get(Uri.parse('https://api.football-data.org/v2/teams/61'),
+            // Send authorization headers to the backend.
+            headers: {
+          'X-Auth-Token': 'df696478a0754c2587c5b849289e4dcb',
+        });
+
+    http.Response response2 = await http.get(
+        Uri.parse('https://api.football-data.org/v2/teams/61/matches'
+            ''),
+        // Send authorization headers to the backend.
+        headers: {
+          'X-Auth-Token': 'df696478a0754c2587c5b849289e4dcb',
+        }
+    );
+
+    Map<String, dynamic> jsonResponse = convert.jsonDecode(response.body);
+    Map<String, dynamic> jsonResponse2 = convert.jsonDecode(response2.body);
+
+    final todaysDate = DateTime.now();
+
+    List<Map<String, dynamic>> l = [];
+
+    for(var i = 0; i < jsonResponse2['matches'].length; i++){
+      print(jsonResponse2['matches'][i]['competition']['name']);
+        print(todaysDate);
+        var gameDate = DateTime.parse(jsonResponse2['matches'][i]['utcDate']);
+        final difference = gameDate.difference(todaysDate).inDays;
+        if((difference >= -30) && (difference <= 0)) {
+            var identifier = { 'away':jsonResponse2['matches'][i]['awayTeam'], 'home':jsonResponse2['matches'][i]['homeTeam'],  'score':jsonResponse2['matches'][i]['score']};
+            l.add(identifier);
+        }
+    }
+
+    List output = [jsonResponse,l];
+
+    return output;
+  }
+
+  String _name(dynamic user) {
+    return user['name'];
+  }
+
+  String _address(dynamic user) {
+    return user['address'];
+  }
+
+  String _logo(dynamic user) {
+    return user['crestUrl'];
+  }
+  String _manager(dynamic user) {
+    return user['away'];
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("jsonResponse"),
+      ),
+      body:
+        Container(
+        child: FutureBuilder<List>(
+          future: func_name(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              print("Check ${_name(snapshot.data[0])}");
+              print("Check ${_address(snapshot.data[0])}");
+              for(var i = 0; i < snapshot.data[1].length; i++){
+                 print(_manager(snapshot.data[1][i]));
+              }
+
+              final Widget networkSvg = SvgPicture.network(
+                  _logo(snapshot.data[0]),
+                semanticsLabel: 'Logo',
+                placeholderBuilder: (BuildContext context) => Container(
+                    padding: const EdgeInsets.all(30.0),
+                    child: const CircularProgressIndicator()),
+              );
+
+
+              return Center(
+                  child: Column(children: <Widget>[networkSvg,
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: Table(
+                    defaultColumnWidth: FixedColumnWidth(120.0),
+                    border: TableBorder.all(
+                        color: Colors.black,
+                        style: BorderStyle.solid,
+                        width: 2),
+                    children: [
+                      TableRow(children: [
+                        Column(children: [
+                          Text('Details', style: TextStyle(fontSize: 20.0))
+                        ]),
+                        Column(children: [
+                          Text('Value', style: TextStyle(fontSize: 20.0))
+                        ]),
+                      ]),
+                      TableRow(children: [
+                        Column(children: [Text('Team Name')]),
+                        Column(children: [Text(_name(snapshot.data[0]))]),
+                      ]),
+                      TableRow(children: [
+                        Column(children: [Text('Manager')]),
+                        Column(children: [Text('Tuchel')]),
+                      ]),
+                      TableRow(children: [
+                        Column(children: [Text('Wins')]),
+                        Column(children: [Text('5')])
+                      ]),
+                      TableRow(children: [
+                        Column(children: [Text('Draws')]),
+                        Column(children: [Text('1')])
+                      ]),
+                      TableRow(children: [
+                        Column(children: [Text('Loses')]),
+                        Column(children: [Text('0')])
+                      ]),
+                      TableRow(children: [
+                        Column(children: [Text('Points')]),
+                        Column(children: [Text('16')])
+                      ]),
+                      TableRow(children: [
+                        Column(children: [Text('Address')]),
+                        Column(children: [Text(_address(snapshot.data[0]))])
+                      ]),
+                    ],
+                  ),
+                )
+              ]));
+            } else {
+
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
+      // _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
     );
   }
 }
